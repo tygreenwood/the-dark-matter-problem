23a2a4eea73dea6fb5df9d363c2984640205e8b7
Tyler Greenwood <t.r.greenwood.pdx@gmail.com>
2023-12-08 19:15:34 -0800

    Fixes pixel blurring

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,20 @@ const COLOR_BACKGROUND: Color = Color::rgb(0.29, 0.31, 0.41);
 fn main() {
     App::new()
         .insert_resource(ClearColor(COLOR_BACKGROUND))
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "The Dark Matter Problem".to_string(),
-                resizable: true,
-                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
-                mode: WindowMode::BorderlessFullscreen,
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "The Dark Matter Problem".to_string(),
+                        resizable: true,
+                        resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                        mode: WindowMode::BorderlessFullscreen,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
         .add_plugins((
             RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.),
             RapierDebugRenderPlugin::default(),
