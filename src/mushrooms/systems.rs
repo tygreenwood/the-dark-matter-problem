c1cd4ccb527a76c33b6edfada1a3c7c2ff2148cd
Rylan Harwood <harwoory@oregonstate.edu>
2023-12-09 23:30:21 -0800

    background updated

--- src/mushrooms/systems.rs
+++ src/mushrooms/systems.rs
@@ -1,12 +1,18 @@
 use bevy::{
+    ecs::{entity::Entity, query::With},
     prelude::{default, AssetServer, Assets, Commands, Query, Res, ResMut, Transform, Vec2, Vec3},
     sprite::{SpriteSheetBundle, TextureAtlas, TextureAtlasSprite},
     time::{Time, Timer, TimerMode},
 };
+use bevy_rapier2d::{
+    dynamics::RigidBody,
+    geometry::{ActiveEvents, Collider, Sensor},
+    plugin::RapierContext,
+};
 
-use crate::{setup::WINDOW_BOTTOM_Y, platforms::FLOOR_THICKNESS};
+use crate::{platforms::FLOOR_THICKNESS, player::components::Player, setup::WINDOW_BOTTOM_Y};
 
-use super::components::{AnimationIndices, AnimationTimer};
+use super::components::{AnimationIndices, AnimationTimer, Mushroom};
 
 pub fn animate_sprite(
     time: Res<Time>,
@@ -38,14 +44,37 @@ pub fn setup_mushrooms(
         TextureAtlas::from_grid(texture_handle, Vec2::new(13.0, 9.0), 31, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     let animation_indices = AnimationIndices { first: 0, last: 30 };
-    commands.spawn((
-        SpriteSheetBundle {
-            texture_atlas: texture_atlas_handle,
-            sprite: TextureAtlasSprite::new(0),
-            transform: Transform {translation: Vec3::new(0., WINDOW_BOTTOM_Y + FLOOR_THICKNESS*3., 0.), scale: Vec3::new(5., 5., 1.), ..default()},
-            ..default()
-        },
-        animation_indices,
-        AnimationTimer(Timer::from_seconds(0.03, TimerMode::Repeating)),
-    ));
+    commands
+        .spawn((
+            SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                sprite: TextureAtlasSprite::new(0),
+                transform: Transform {
+                    translation: Vec3::new(0., WINDOW_BOTTOM_Y + FLOOR_THICKNESS * 3., 0.),
+                    scale: Vec3::new(5., 5., 1.),
+                    ..default()
+                },
+                ..default()
+            },
+            animation_indices,
+            AnimationTimer(Timer::from_seconds(0.03, TimerMode::Repeating)),
+            RigidBody::Fixed,
+            Collider::cuboid(0.5, 0.5),
+        ))
+        .insert(Sensor)
+        .insert(ActiveEvents::CONTACT_FORCE_EVENTS);
+}
+
+pub fn check_mushroom_hop(
+    rapier_context: Res<RapierContext>,
+    player_query: Query<Entity, With<Player>>,
+    mushroom_query: Query<Entity, With<Mushroom>>,
+) {
+    let player = player_query.single();
+
+    for mushroom in &mushroom_query {
+        if rapier_context.intersection_pair(player, mushroom) == Some(true) {
+            println!("intersecting!!")
+        }
+    }
 }
