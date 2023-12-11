d5473cc489686a2e25f5d64638bc94a4768ea322
Rylan Harwood <harwoory@oregonstate.edu>
2023-12-10 20:57:51 -0800

    cleaned up the assets folder and updated the background

--- src/background/systems.rs
+++ src/background/systems.rs
@@ -1,6 +1,7 @@
 use bevy::{
     prelude::{default, AssetServer, Commands, GlobalTransform, Query, Res, Transform, Vec3, With},
     sprite::SpriteBundle,
+    time::Time, // remove this if we get rid of the animated backgrounds
 };
 
 use crate::player::components::Player;
@@ -10,7 +11,7 @@ use super::components::Background;
 pub fn setup_background(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         SpriteBundle {
-            texture: asset_server.load("BackgroundTest2(expanded).png"),
+            texture: asset_server.load("background/Background_2.png"),
             transform: Transform {
                 translation: Vec3::new(0., 0., -3.),
                 scale: Vec3::new(5., 5., 1.),
@@ -18,9 +19,32 @@ pub fn setup_background(mut commands: Commands, asset_server: Res<AssetServer>) 
             },
             ..default()
         },
+        //animation_indicies,
+        //AnimationTimer(Timer::from_seconds(0.03, TimerMode::Repeating)),
         Background,
     ));
 }
+// ok listen this is in the middle of not working, switching the background
+// to a non animated one in the meantime thx
+// pub fn animate_sprite(
+//     time: Res<Time>,
+//     mut query: Query<(
+//         &AnimationIndices,
+//         &mut AnimationTimer,
+//         &mut TextureAtlasSprite,
+//     )>,
+// ) {
+//     for (indices, mut timer, mut sprite) in &mut query {
+//         timer.tick(time.delta());
+//         if timer.just_finished() {
+//             sprite.index = if sprite.index == indices.last {
+//                 indices.first
+//             } else {
+//                 sprite.index + 1
+//             };
+//         }
+//     }
+// }
 
 pub fn move_background(
     query_player: Query<&GlobalTransform, With<Player>>,
--- src/player/systems.rs
+++ src/player/systems.rs
@@ -18,7 +18,7 @@ pub fn setup_player(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let texture_handle = asset_server.load("SpaceGuyMk5RunningAnimation1.png");
+    let texture_handle = asset_server.load("player_sprites/SpaceGuyMk5RunningAnimation1.png");
     let texture_atlas =
         TextureAtlas::from_grid(texture_handle, Vec2::new(19.0, 32.0), 7, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
